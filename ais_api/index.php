6c826ac408658ba7cf263a2218c66fa34ffb81be
Fedorin Mykyta <xfedorin@node34.webte.fei.stuba.sk>
2024-03-31 23:51:02 +0200

    Parsing subjects to objects

--- ais_api/index.php
+++ ais_api/index.php
@@ -1,1 +1,41 @@
-<?php echo "ais apiiiiiiiii"?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Download Schedule</title>
+    <!-- Подключаем библиотеку jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+<body>
+    <h2>Download Schedule</h2>
+    <!-- Создаем кнопку для вызова скрипта download_schedule.php -->
+    <button id="downloadBtn">Download Schedule</button>
+
+    <!-- Создаем блок для отображения результата загрузки расписания -->
+    <div id="scheduleResult">
+        <p id="resultText"></p> <!-- Тег для вывода полученного текста -->
+    </div>
+
+    <script>
+        // При нажатии на кнопку с id="downloadBtn"
+        $('#downloadBtn').click(function() {
+            // Выполняем AJAX-запрос к скрипту download_schedule.php
+            $.ajax({
+                url: 'download_schedule.php', // URL-адрес вашего скрипта
+                type: 'GET', // Метод запроса
+                dataType: 'text', // Тип данных, который ожидается в ответе
+                success: function(response) {
+                    // Обновляем содержимое тега с id="resultText" полученным текстом
+                    $('#resultText').text(response);
+                },
+                error: function(xhr, status, error) {
+                    // Обработка ошибки
+                    console.error(error); // Выводим сообщение об ошибке в консоль
+                }
+            });
+        });
+    </script>
+</body>
+</html>
+
--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-<?php echo 'hello world'?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index Page</title>
+</head>
+<body>
+    <h2>Ссылки на другие страницы:</h2>
+    <!-- Ссылка на ais_api/index.php -->
+    <a href="ais_api/index.php">Перейти на страницу ais_api</a><br>
+    <!-- Ссылка на zaverecne_prace/index.php -->
+    <a href="zaverecne_prace/index.php">Перейти на страницу zaverecne_prace</a>
+</body>
+</html>
+
