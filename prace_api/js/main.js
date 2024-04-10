ef483a6d77c44e07a48f1e3a591fca8831decdea
nikita <nikita@debian.nikitafedorin.com>
2024-04-10 21:14:06 +0200

    thesis topic link

--- prace_api/js/main.js
+++ prace_api/js/main.js
@@ -3,52 +3,61 @@ async function fetchData() {
     const thesisType = $("#thesisTypeSelect").val();
     const requestData = {
         department: dep,
-        thesis_type: thesisType
+        thesis_type: thesisType,
     };
 
     try {
-        const response = await fetch('/prace_api/prace', {
-            method: 'POST',
+        const response = await fetch("/prace_api/prace", {
+            method: "POST",
             headers: {
-                'Content-Type': 'application/json'
+                "Content-Type": "application/json",
             },
-            body: JSON.stringify(requestData)
+            body: JSON.stringify(requestData),
         });
 
         if (!response.ok) {
-            throw new Error('Network response was not ok');
+            throw new Error("Network response was not ok");
         }
 
         const responseData = await response.json();
-        $('#loader').hide();
-        console.log(response);
-        console.log(responseData);
+        $("#loader").hide();
 
-        if ($.fn.DataTable.isDataTable('#thesisTable')) {
+        if ($.fn.DataTable.isDataTable("#thesisTable")) {
             // DataTables already initialized, destroy it first
-            $('#thesisTable').DataTable().destroy();
+            $("#thesisTable").DataTable().destroy();
         }
-        $('#thesisTable').DataTable({
+        $("#thesisTable").DataTable({
             data: responseData,
             columns: [
-                { data: 'topic' },
-                { data: 'supervisor' },
-                { data: 'programme' }
-            ]
+                {
+                    data: "topic",
+                    render: function (data, type, row) {
+                        // Проверяем, что это отображение в таблице
+                        if (type === "display") {
+                            // Создаем ссылку с названием темы
+                            return `<a href="${row.abstractUrl}">${data}</a>`;
+                        }
+                        // Для остальных случаев возвращаем исходные данные
+                        return data;
+                    },
+                },
+                { data: "supervisor" },
+                { data: "programme" },
+            ],
         });
     } catch (error) {
-        $('#loader').hide();
+        $("#loader").hide();
         console.error(error.message);
     }
 }
 
 // Получаем кнопку по id
-const refreshButton = document.getElementById('refreshButton');
+const refreshButton = document.getElementById("refreshButton");
 
 // Добавляем обработчик события click
-refreshButton.addEventListener('click', async () => {
+refreshButton.addEventListener("click", async () => {
     // Вызываем функцию fetchData() при клике на кнопку
     await fetchData();
 });
 fetchData();
-$('#myTable').DataTable();
+$("#myTable").DataTable();
